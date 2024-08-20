0fefaef37fb0edcee8ce134ab933886c1f98d2d7
eriknathan <eriknathan.contato@gmail.com>
2024-08-20 13:15:27 -0300

    update: kind

--- install_kind.sh
+++ install_kind.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# Verifica se a arquitetura é x86_64
 echo "Verificando a arquitetura da máquina..."
+echo "--------------------------------------------"
+
 if [ "$(uname -m)" = "x86_64" ]; then
+
   echo "Arquitetura x86_64 detectada. Baixando o binário do Kind para Linux 64-bit..."
-  
-  # Baixa o binário do Kind para Linux 64-bit
+  echo "--------------------------------------------"
   curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-amd64
   
   echo "Tornando o binário do Kind executável..."
-  
-  # Torna o binário executável
+  echo "--------------------------------------------"
   chmod +x ./kind
   
   echo "Movendo o binário do Kind para /usr/local/bin..."
-  
-  # Move o binário para /usr/local/bin
+  echo "--------------------------------------------"
   sudo mv ./kind /usr/local/bin/kind
 
   echo "Kind instalado com sucesso!"
+  echo "--------------------------------------------"
 else
   echo "Arquitetura não suportada. Este script suporta apenas arquiteturas x86_64."
+  echo "--------------------------------------------"
 fi
